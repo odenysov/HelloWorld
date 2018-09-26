df50149c9dffcf027c80e4a44b16c87076896314
Oleksandr Denysov <odeny@softserveinc.com>
2018-09-26 13:49:47 +0300

    First commit

---
+++ HelloWorld/HelloWorld/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HelloWorld")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HelloWorld")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("db4ce6fb-fabf-44aa-b614-ddba59b03840")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ HelloWorld/HelloWorld/UnitTest1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace HelloWorld
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        ChromeDriver driver = new ChromeDriver();
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+            driver.Manage().Window.Maximize();
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+
+            driver.Navigate().GoToUrl("https://perf.exalinkservices.com/auth/?redirect=https%3A%2F%2Fperf.exalinkservices.com%2Fcalcs%2F%23%2Fprojects");
+
+            var login = driver.FindElementById("username");
+            var password = driver.FindElementByXPath("//input[@placeholder = 'Password']");
+            var logIn = driver.FindElementByXPath("//button[@class = 'btn btn-block']");
+
+            login.SendKeys("jack");
+            password.SendKeys("Dummy#123");
+
+            logIn.Click();
+
+
+            driver.FindElementByXPath("//button[@id='clients']").Click();
+            driver.FindElementByXPath("//span[text()='Medicines']").Click();
+
+            Assert.IsTrue(IsPresent("//button[@class='btn btn-primary view-project']"));
+
+
+            driver.FindElementByXPath("//button[@id = 'nav-item-data']").Click(); //Data Page
+            driver.FindElementByXPath("//td[@class = 'cell-success']").Click();//first row in grid
+
+
+            var buttonView = driver.FindElementByXPath("//button[@ng-click = 'ctrl.viewData()']");
+
+            ReadOnlyCollection<IWebElement> elements = driver.FindElements(By.XPath("//button[@ng-click = 'ctrl.viewData()']"));
+
+            foreach (IWebElement element in elements)
+            {
+                if (element.Displayed)
+                {
+                    buttonView = element;
+                    break;
+                }
+            }
+
+            buttonView.Click();
+
+            Assert.IsTrue(IsPresent("//div[@class='ag-body-viewport']"));
+
+            driver.Navigate().Back();
+
+            driver.FindElementByXPath("//a[contains(. , 'Domain')]").Click();
+
+            
+
+            buttonView.Click();
+
+            Assert.IsTrue(IsPresent("//div[@class='ag-body-viewport']"));
+
+
+        }
+        public bool IsPresent(string path)
+        {
+            bool present;
+            try {
+                driver.FindElementByXPath(path);
+                present = true;
+            }
+            catch (NoSuchElementException)
+            {
+                present = false;
+            }
+            return present;
+        }
+    }
+}
